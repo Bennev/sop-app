c249aa4f0826dd0ae43b27fa16af7083d5585421
Matheus Benevides <matheus.benevides@hublocal.com.br>
2025-03-26 09:13:05 -0300

    [fix/SOP-12] Fixing loading position

--- src/app/(private)/expenses/styles.ts
+++ src/app/(private)/expenses/styles.ts
@@ -2,9 +2,11 @@
 import styled from "styled-components";
 
 export const StyledLoading = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const StyledContainer = styled.div`
